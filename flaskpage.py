6d469f71dc605758a9cf4fe557ec72436a3b92dc
garywu007 <46955710+garywu007@users.noreply.github.com>
2020-04-02 01:05:24 +0800

    Update flaskpage.py

--- flaskpage.py
+++ flaskpage.py
@@ -1,11 +1,16 @@
+import sys
 from flask import Flask
 
 app = Flask(__name__)
 
+print("initial")
+
 @app.route('/')
 def hello_world():
+    print("index")
     return 'Hello, World!'
 
 @app.route('/test')
 def test_page():
+    print("/test")
     return 'In test page!'
