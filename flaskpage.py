0529c43d326e1464d542a8907b40f6c0a9136992
garywu007 <46955710+garywu007@users.noreply.github.com>
2020-04-02 17:23:47 +0800

    Update flaskpage.py

--- flaskpage.py
+++ flaskpage.py
@@ -1,8 +1,6 @@
 import sys
 from flask import Flask, request
-from django.template import Template, Context
-from django.conf import settings
-settings.configure()
+from yattag import Doc
 
 app = Flask(__name__)
 
@@ -16,15 +14,32 @@ your favourate {{ class }} is {{item}}
 </body>
 </html>
 """
+doc, tag, text = Doc().tagtext()
+
+with tag('html'):
+    with tag('body'):
+        with tag('p', id = 'main'):
+            text('some text')
+        with tag('a', href='/my-url'):
+            text('some link')
+
+result = doc.getvalue()
 
 @app.route('/')
 def hello_world():
     cls1 = request.args.get('cls', default = '-', type = str)
     item = request.args.get('item', default = '-', type = str)
     
-    t = Template(index_html)
-    c = Context({"class": cls1, "item":item})
-    return t.render(c)
+    doc, tag, text = Doc().tagtext()
+
+    with tag('html'):
+        with tag('body'):
+        with tag('body'):
+            with tag('p', id = 'main'):
+                text('your favourate ' + cls1 + ' is ' + item)
+
+    result = doc.getvalue()
+    return result
 
 @app.route('/test')
 def test_page():
