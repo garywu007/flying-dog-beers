9cfe90db511128a6844dfad8161b7b1b571cd71e
garywu007 <46955710+garywu007@users.noreply.github.com>
2020-04-02 16:21:23 +0800

    Update flaskpage.py

--- flaskpage.py
+++ flaskpage.py
@@ -3,12 +3,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
-print("initial")
+index_html = """
+<html>
+<head>
+<title>i know what's your favourate {{ class }}</title>
+</head>
+<body>
+your favourate {{ class }} is {{item}}
+</body>
+</html>
+"""
 
 @app.route('/')
 def hello_world():
-    print("index")
-    return 'Hello, World!'
+    cls1 = request.GET.get('cls')
+    item = request.GET.get('item')
+    
+    t = Template(index_html)
+    c = Context({"class": cls1, "item":item})
+    return t.render(c)
 
 @app.route('/test')
 def test_page():
